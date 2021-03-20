9ab90f466b6e6a187bef5a37dd04e14c585b2e54
Debid Magar <linkwithdm@gmail.com>
2021-03-20 18:02:44 +0545

    auth added

--- modules/auth/auth.route.js
+++ modules/auth/auth.route.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const authController = require("./auth.cotroller");
 
-router.route("/login").post(userController.userAuthenticate);
+router.route("/login").post(authController.userAuthenticate);
+router.route("/register").post(authController.userRegister);
+router.route("/forgotpassword").post(authController.forgotPassword);
 
 module.exports = router;
